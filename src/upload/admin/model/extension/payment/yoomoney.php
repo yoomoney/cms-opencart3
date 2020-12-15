62898f2a29bd09138fadf0e0178d710c476da04d
Антон Н. Николаев <nikolaevanym@yoomoney.ru>
2020-12-15 10:02:13 +0300

    Новый виджет кредитования

--- src/upload/admin/model/extension/payment/yoomoney.php
+++ src/upload/admin/model/extension/payment/yoomoney.php
@@ -5,7 +5,7 @@ class ModelExtensionPaymentYoomoney extends Model
     /**
      * string
      */
-    const MODULE_VERSION = '2.0.0';
+    const MODULE_VERSION = '2.0.1';
     const YOOMONEY_EVENT_SECOND_RECEIPT_CODE = 'yoomoney_second_receipt_trigger';
 
     private $kassaModel;
